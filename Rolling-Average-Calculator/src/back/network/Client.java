5da073b8a8434a2fce144210c5dab14cbd16447c
Andy Tse <atse4995@gmail.com>
2017-11-16 14:55:43 -0500

    [6][9] Client message sending and Multi-threaded server
    -Clients can now send numbers ( or any input as of right now) to the server.
    -Refactored Server and Client
    -ServerAdapter and ClientAdapater has been added to help bridge the UI and back.
    -Created map of ClientID and ClientConnections
    -Reorganized code to allow better readability and better structure and comments.

--- Rolling-Average-Calculator/src/back/network/Client.java
+++ Rolling-Average-Calculator/src/back/network/Client.java
@@ -1,7 +1,5 @@
 package back.network;
 
-import back.interfacing.ClientUI;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,93 +7,92 @@ import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
-/**
- * This class is used to create a network connection to the {@link Server}
- * for a user to enter numbers.
- */
-public class Client {
+public class Client implements Runnable {
 
-    BufferedReader in;
-    PrintWriter out;
+    private PrintWriter out;
+    private BufferedReader in;
 
-    /**
-     * The interface that a user will use to communicate with this. It is
-     * preferred to communicate back to this handler on a non-UI thread.
-     */
-    private ClientUI UIHandler;
+    private int clientPort;
+    private String clientAddress;
+    private Socket clientSocket;
+
+    private int clientID;
 
     /**
-     * Establish a link to the communication interface that the user
-     * will use.
-     * @param handler The communication interface being used.
-     */
-    public void setUIHandler(final ClientUI handler) {
-        UIHandler = handler;
-    }
-
-    /**
-     * Attempt to create a network connection to a {@link Server}. Whether this
-     * succeeds or fails, this method should communicate back to the
-     * {@link ClientUI}.
+     * Creates new Client with address and port
      *
-     * @param ipAddress The IPv4 or IPv6 network address to connect to.
-     * @param port The port of the server to connect to.
+     * @param address The IPv4 or IPv6 address to create the network server.
+     * @param port    The port number to open for the network server.
      */
-    public void connect(final String ipAddress, final String port) {
-        System.out.println("Attempting to connect to " + ipAddress
-            + ":" + port);
-
-        new Thread(()-> {
-            try {
-                Socket clientSocket = new Socket();
-                clientSocket.connect(new InetSocketAddress(ipAddress, Integer.parseInt(port)), 2000);
-                UIHandler.onConnectionSuccess();
-
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-            } catch (IOException e) {
-                UIHandler.onConnectionFailure("Failed to open socket");
-            }
-        }).start();
-
-
-
-
-//        // FIXME 11/07/17: Fill in method and remove the random calls.
-//        if (UIHandler != null) {
-//            new Thread(() -> {
-//                // Simulate a network delay.
-//                try {
-//                    Thread.sleep(5000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                final boolean didConnect =  Math.random() > 0.99f;
-//                if (didConnect) {
-//                    UIHandler.onConnectionSuccess();
-//                } else {
-//                    UIHandler.onConnectionFailure(
-//                        "You were unlucky, I guess.");
-//                }
-//            }).start();
-//        } else {
-//            System.err.println("Attempted to connect without user interface.");
-//            System.exit(-1);
-//        }
+    public Client(String address, int port) {
+        this.clientPort = port;
+        this.clientAddress = address;
     }
 
     /**
-     * Close any existing connections and clean up variables so any connected
-     * {@link Server} will not throw exceptions when this program ends.
+     * Opens the socket and establishes IO with server
      */
-    public void disconnect() {
-        // FIXME 11/07/17: Fill this method in correctly.
-        System.out.println("Disconnected!");
+    public void run() {
+
+        openClientSocket();
+
+        try {
+            this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+
+            System.out.println(in.readLine());
+            System.out.println(clientID = Integer.parseInt(in.readLine()));
+
+            out.print("Hello");
+            out.flush();
+
+            //TODO Create a listener to receive messages from the server
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    @Override
-    public String toString() {
-        // TODO 11/07/17: Remove this toString or make it meaningful.
-        return this.getClass().getSimpleName();
+    public void messageToServer(String message) {
+        this.out.println(message);
+        this.out.flush();
+    }
+
+    /**
+     * Shutdowns the client connection with the server
+     */
+    public void terminate() {
+        try {
+            this.in.close();
+            this.out.close();
+
+            //TODO Send message to server to close this client with this ID
+
+            this.clientSocket.close();
+            if (clientSocket.isClosed()) {
+                //TODO
+                //UIHandler.onShutdownSuccess();
+            }
+        } catch (IOException e) {
+            //TODO
+            //UIHandler.onShutdownFailure("Error in closing connection");
+            System.out.println("Error in closing connection");
+        }
+    }
+
+    /**
+     * Opens the client socket with designated port and address
+     */
+    private void openClientSocket() {
+        try {
+            this.clientSocket = new Socket();
+            this.clientSocket.connect(new InetSocketAddress(clientAddress, clientPort), 2000);
+        } catch (IOException e) {
+            //TODO
+//            UIHandler.onConnectionFailure("Failed to open socket");
+            System.out.println("Failed to open socket");
+        }
     }
 }
--- Rolling-Average-Calculator/src/back/network/Server.java
+++ Rolling-Average-Calculator/src/back/network/Server.java
@@ -1,87 +1,116 @@
 package back.network;
 
-import back.interfacing.ServerUI;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- * This class is used to start a network connection for a {@link Client} to
- * link to. It will perform various actions and communicate back to the
- * {@link Client}.
- */
-public class Server {
+public class Server implements Runnable{
 
-    /**
-     * The interface that a user will use to communicate with this. It is
-     * preferred to communicate back to this handler on a non-UI thread.
-     */
-    private ServerUI UIHandler;
-    private boolean isShuttingDown = false;
-    private ServerPool server;
-    private Thread threadServer;
+    private int serverPort;
+    private String serverAddress;
 
-    /**
-     * Establish a link to the communication interface that the user
-     * will use.
-     * @param handler The communication interface being used.
-     */
-    public void setUIHandler(final ServerUI handler) {
-        UIHandler = handler;
-    }
+    private ServerSocket serverSocket = null;
+    private ExecutorService threadPool = Executors.newFixedThreadPool(4);
+
+    private boolean isStopped = false;
+    private Map<Integer,InetAddress> clientIdConnection = new HashMap<Integer,InetAddress>();
+    private AtomicInteger clientId = new AtomicInteger(0);
+
 
     /**
-     * Create the network server and start listening for {@link Client}s.
+     * Creates new Server with address and port
      *
-     * @param ipAddress The IPv4 or IPv6 address to create the network server.
+     * @param address The IPv4 or IPv6 address to create the network server.
      * @param port The port number to open for the network server.
      */
-    public void spinUp(final String ipAddress, final String port) {
-        new Thread(()-> {
-            server = new ServerPool(Integer.parseInt(port));
-            threadServer = new Thread(server);
-            threadServer.start();
-            if (threadServer.isAlive()) {
-                UIHandler.onSpinUpSuccess();
+    public Server( String address, int port ){
+        this.serverPort = port;
+        this.serverAddress = address;
+    }
+
+
+    /**
+     * Creates new thread for every new connection from clients
+     */
+    @Override
+    public void run() {
+
+        openServerSocket();
+
+        while(!isStopped){
+            Socket clientSocket;
+            try{
+                clientSocket = this.serverSocket.accept();
+            }catch (IOException e){
+                if(isStopped){
+                    System.out.println("ServerAdapter has stopped");
+                    break;
+                }
+                throw new RuntimeException("Error accepting client connection", e);
             }
-        }).start();
+            System.out.println("New client: "+clientSocket.getLocalAddress());
+            int curClientID=clientId.getAndIncrement();
+            clientIdConnection.put(curClientID,clientSocket.getLocalAddress());
+            this.threadPool.execute(new ServerThread(clientSocket,curClientID));
+        }
+
     }
 
     /**
-     * Go through all {@link Client}'s and disconnect from them safely so
-     * they can exit properly.
+     * Shutdown the Server by closing the thread pool and closing
+     * the sockets of Client and Servers
      */
-    public synchronized void shutDown() {
-        if (isShuttingDown) {
-            return;
-        }
-        isShuttingDown = true;
+    public void terminate(){
 
-//        // FIXME 11/07/17: Fill this method in correctly.
-//        new Thread(()->{
-//            for (int i = 0; i < 3; ++i) {
-//                try {
-//                    Thread.sleep(2000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                // Regardless of whether the UIHandler is null, we want
-//                // to shutdown all connections since we know we may exit.
-//                if (UIHandler != null) {
-//                    UIHandler.onClientDisconnected("127.0.0.1");
-//                }
-//            }
-//            if (UIHandler != null) {
-//                final boolean isSuccessful = Math.random() < 0.99f;
-//                if (isSuccessful) {
-//                    UIHandler.onShutdownSuccess();
-//                } else {
-//                    UIHandler.onShutdownFailure("You were unlucky.");
-//                }
-//            }
-//        }).start();
+        this.isStopped = true;
+
+        try {
+            this.threadPool.shutdown();
+            this.serverSocket.close();
+
+            this.threadPool.awaitTermination(60000, TimeUnit.NANOSECONDS);
+            System.out.println("ServerAdapter Stopped.") ;
+
+        } catch (IOException e) {
+            //TODO
+//            UIHandler.onShutdownFailure("Error closing server");
+            System.out.println("Error closing server");
+        } catch (InterruptedException e) {
+            //TODO
+//            UIHandler.onShutdownFailure("Server shutdown was interrupted");
+            System.out.println("Server shutdown was interrupted");
+        }
+
+        //TODO Shutdown each clientSocket gracefully by alerting each client
     }
 
-    @Override
-    public String toString() {
-        // TODO 11/07/17: Remove this toString or make it meaningful.
-        return this.getClass().getSimpleName();
+    /**
+     * Opens the server socket with designated port and address
+     */
+    private void openServerSocket() {
+
+        try {
+
+            InetAddress address = InetAddress.getByName(this.serverAddress);
+            this.serverSocket = new ServerSocket(this.serverPort,50,address);
+
+        }catch (UnknownHostException e){
+//            UIHandler.onSpinUpFailure("Could not get host " + serverAddress);
+            //TODO
+            System.out.println("Could not get host: "+serverAddress);
+        }catch (IOException e) {
+//            UIHandler.onSpinUpFailure("Could not open server port");
+            //TODO
+            System.out.println("Could not open server port "+serverPort);
+        }
+
     }
 }
