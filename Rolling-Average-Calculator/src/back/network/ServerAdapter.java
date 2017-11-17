7e3ec9a8d9b1202022f5a0e25ddcfdf500b42e41
Andy Tse <atse4995@gmail.com>
2017-11-16 20:23:59 -0500

    [2][3] Client and Server
    -Created handler interfaces for both client and server
    -Added TODOs for priority tasks

--- Rolling-Average-Calculator/src/back/network/ServerAdapter.java
+++ Rolling-Average-Calculator/src/back/network/ServerAdapter.java
@@ -2,12 +2,14 @@ package back.network;
 
 import back.interfacing.ServerUI;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * This class is used to start a network connection for a {@link ClientAdapter} to
  * link to. It will perform various actions and communicate back to the
  * {@link ClientAdapter}.
  */
-public class ServerAdapter {
+public class ServerAdapter implements Server.ServerHandler{
 
     /**
      * The interface that a user will use to communicate with this. It is
@@ -17,6 +19,8 @@ public class ServerAdapter {
     private boolean isShuttingDown = false;
     private Server server;
     private Thread threadServer;
+
+    private ReentrantLock stateLock;
 
     /**
      * Establish a link to the communication interface that the user
@@ -35,10 +39,9 @@ public class ServerAdapter {
      */
     public void spinUp(final String ipAddress, final String port) {
         new Thread(()-> {
-
-
-
+            stateLock = new ReentrantLock();
             server = new Server(ipAddress,Integer.parseInt(port));
+            server.setSSHandler(this);
             threadServer = new Thread(server);
             threadServer.start();
             if (threadServer.isAlive()) {
@@ -59,11 +62,86 @@ public class ServerAdapter {
         }
         isShuttingDown = true;
 
+
+
         if(server.terminate()){
             UIHandler.onShutdownSuccess();
+            //TODO Terminate server itself
         }else{
             UIHandler.onShutdownFailure("Could not not close all connections regarding server");
         }
     }
 
+    @Override
+    public void onOpenSocketSuccess() {
+        while (!stateLock.isHeldByCurrentThread()) {
+            stateLock.lock();
+        }
+        final String success = "Server Socket successfully opened";
+        System.out.println(success);
+        stateLock.unlock();
+    }
+
+    @Override
+    public void onOpenSocketFailure(String reason) {
+        while (!stateLock.isHeldByCurrentThread()) {
+            stateLock.lock();
+        }
+        final String failure = "Server Socket failed to opened due to: " + reason;
+        System.out.println(failure);
+
+        //TODO Terminate server immediately
+
+        stateLock.unlock();
+    }
+
+    @Override
+    public void onClientConnected(String ipAddress, int clientID) {
+        while (!stateLock.isHeldByCurrentThread()) {
+            stateLock.lock();
+        }
+        final String connection = "ClientID: " + clientID + " - Client IP Address: " +ipAddress+ " connected";
+        System.out.println(connection);
+        stateLock.unlock();
+    }
+
+    @Override
+    public void onClientDisconnected(String ipAddress,int clientID) {
+        while (!stateLock.isHeldByCurrentThread()) {
+            stateLock.lock();
+        }
+        final String connection = "ClientID: " + clientID + " - Client IP Address: " +ipAddress+ " disconnected";
+        System.out.println(connection);
+        stateLock.unlock();
+    }
+
+    @Override
+    public void onShutdownSuccess() {
+        while (!stateLock.isHeldByCurrentThread()) {
+            stateLock.lock();
+        }
+        final String success = "Server Thread successfully shutdown";
+        System.out.println(success);
+        stateLock.unlock();
+    }
+
+    @Override
+    public void onShutdownFailure(String reason) {
+        while (!stateLock.isHeldByCurrentThread()) {
+            stateLock.lock();
+        }
+        final String failure = "Server Thread failed to shutdown due to: " + reason;
+        System.out.println(failure);
+        stateLock.unlock();
+    }
+
+    @Override
+    public void onConnectionBroken(String reason) {
+        while (!stateLock.isHeldByCurrentThread()) {
+            stateLock.lock();
+        }
+        final String failure = "Server Connection broken due to: " + reason;
+        System.out.println(failure);
+        stateLock.unlock();
+    }
 }
